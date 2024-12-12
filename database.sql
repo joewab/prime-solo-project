beb6a15654b1bed9aa28a64e460c8bb10656b1e6
Joe AB <joe.anthonybrown@gmail.com>
2024-12-11 18:47:50 -0600

    changing database structure

--- database.sql
+++ database.sql
@@ -1,61 +1,60 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" serial NOT NULL,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"admin" boolean NOT NULL,
+	PRIMARY KEY ("id")
 );
 
-CREATE TABLE "image" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS "image" (
+	"id" serial NOT NULL,
 	"url" varchar(255555) NOT NULL,
 	"description" varchar(2555) NOT NULL,
 	"artist" varchar(255) NOT NULL,
 	"title" varchar(255) NOT NULL,
-	"year" varchar(255) NOT NULL,
-	"media" varchar(255) NOT NULL);
+	"year" bigint NOT NULL,
+	"medium" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "gallery" (
+	"id" serial NOT NULL,
+	"name" varchar(255) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "gallery_image" (
+	"id" serial NOT NULL,
+	"gallery_id" bigint NOT NULL,
+	"image_id" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "Score" (
+	"id" serial NOT NULL,
+	"score" bigint NOT NULL,
+	"user_id" bigint NOT NULL,
+	"gallery_id" bigint NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE IF NOT EXISTS "user_gallery" (
+	"id" serial NOT NULL UNIQUE,
+	"user_id" bigint NOT NULL,
+	"gallery_id" bigint NOT NULL,
+	"relationship" varchar(255),
+	PRIMARY KEY ("id")
+);
 
 
 
-CREATE TABLE "gallery" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"name" varchar(255) NOT NULL);
 
+ALTER TABLE "gallery_image" ADD CONSTRAINT "gallery_image_fk1" FOREIGN KEY ("gallery_id") REFERENCES "gallery"("id");
 
+ALTER TABLE "gallery_image" ADD CONSTRAINT "gallery_image_fk2" FOREIGN KEY ("image_id") REFERENCES "image"("id");
+ALTER TABLE "Score" ADD CONSTRAINT "Score_fk2" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
-CREATE TABLE "classroom" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"class_name" varchar(255) NOT NULL);
+ALTER TABLE "Score" ADD CONSTRAINT "Score_fk3" FOREIGN KEY ("gallery_id") REFERENCES "gallery"("id");
+ALTER TABLE "user_gallery" ADD CONSTRAINT "user_gallery_fk1" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
-
-
-CREATE TABLE "class_gallery" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"class_id" integer NOT NULL,
-	"gallery_id" integer NOT NULL);
-
-
-
-CREATE TABLE "user_class" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"user_id" integer NOT NULL,
-	"class_id" integer NOT NULL);
-
-
-
-CREATE TABLE "gallery_image" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"gallery_id" integer NOT NULL,
-	"image_id" integer NOT NULL);
-
-
-
-CREATE TABLE "score" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"score" integer DEFAULT 0,
-	"user_id" integer DEFAULT 0,
-	"gallery_id" integer DEFAULT 0);
-	
+ALTER TABLE "user_gallery" ADD CONSTRAINT "user_gallery_fk2" FOREIGN KEY ("gallery_id") REFERENCES "gallery"("id");
